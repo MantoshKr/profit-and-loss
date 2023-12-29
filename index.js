cc775bcc1ff3208d353c87bd4670e948c47b008f
mantoshdev <123961664+mantoshdev2@users.noreply.github.com>
2023-12-29 16:56:46 +0530

    feat: Added profit and loss calculator functionality
    Added js logic to calculate profit or loss based on initial price, quantity, and current price.

---
+++ index.js
@@ -0,0 +1,40 @@
+var initialPrice = document.querySelector("#initial-price");
+var stocksQuantity = document.querySelector("#stocks-quantity");
+var currentPrice = document.querySelector("#current-price");
+var submitBtn = document.querySelector("#submit-btn");
+var outputBox = document.querySelector("#output-box");
+
+submitBtn.addEventListener("click", submitHandler);
+
+function submitHandler() {
+  var ip = Number(initialPrice.value);
+  var qty = Number(stocksQuantity.value);
+  var curr = Number(currentPrice.value);
+
+  calculateProfitAndLoss(ip, qty, curr);
+}
+
+
+function calculateProfitAndLoss(initial, quantity, current) {
+  if (initial > current) {
+    var loss = (initial - current) * quantity;
+    var lossPercentage = (loss / initial) * 100;
+
+    showOutput(
+      `Your loss is ${loss} and the percent is ${lossPercentage.toFixed(2)}%`
+    );
+  } else if (current > initial) {
+    var profit = (current - initial) * quantity;
+    var profitPercentage = (profit / initial) * 100;
+
+    showOutput(
+      `Your profit is ${profit} and the percent is ${profitPercentage.toFixed(2)}%`
+    );
+  } else {
+    showOutput(`No Profit or Loss`);
+  }
+}
+
+function showOutput(message) {
+  outputBox.innerHTML = message;
+}
